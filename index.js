633519997fc40230c4dbb472c8a099737c0c156b
PatrikSlat <patrik.slat@gmail.com>
2023-12-30 01:15:17 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+/* Pseudocode: Get players choice, randomize comptuer choice, compare choices, decide who wins the turn
+based on the choice, give the player/computer a point, do while <3 game points, first player to reach 3
+points wins the game.*/
