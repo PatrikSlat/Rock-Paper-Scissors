952cecb55147f0b1b2c54f553d8578b3b7f0e620
PatrikSlat <patrik.slat@gmail.com>
2023-12-30 01:53:50 +0100

    Created basic logic for Computer choice and game logic

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 /* Pseudocode: Get players choice, randomize comptuer choice, compare choices, decide who wins the turn
 based on the choice, give the player/computer a point, do while <3 game points, first player to reach 3
 points wins the game.*/
+
+let playerPoints = 0;
+let computerPoints = 0;
+
+function getComputerChoice(){
+    const computerChoices = ["rock", "paper", "scissors"]
+    //Getting a random number between 1 and 3 to automate the picking choice, using floor to round up
+    let randomIndex = Math.floor(Math.random() * computerChoices.length); 
+    return computerChoices[randomIndex]
+};
+
+
+function getPlayerChoice(){
+    //Eventlistener to get the choice
+};
+
+function compareChoices(){
+    //Logic behind deciding which choice won and point giving 
+};
+
+while(playerPoints < 3 || computerPoints < 3){
+    //Logic behind calling all the function
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+
+};
+
+
+
